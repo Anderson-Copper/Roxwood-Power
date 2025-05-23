13ea58c5e8b2edc41ae28d4ad90c9f2ac34e46e2
Anderson-Copper <duquesne.pierre@outlook.com>
2025-05-23 02:58:22 +0200

    Update register-commands.js

--- register-commands.js
+++ register-commands.js
@@ -2,22 +2,16 @@ require('dotenv').config();
 const { REST, Routes } = require('discord.js');
 
 const CLIENT_ID = process.env.CLIENT_ID_PWR;
-const GUILD_ID = '1363243114822766763';
 const TOKEN = process.env.DISCORD_TOKEN_PWR;
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 (async () => {
   try {
-    console.log('🧹 Suppression de TOUTES les commandes du bot dans le serveur...');
-
-    await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-      { body: [] }
-    );
-
-    console.log('✅ Toutes les commandes du serveur ont été supprimées !');
+    console.log('🧹 Suppression de toutes les commandes globales...');
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: [] });
+    console.log('✅ Toutes les commandes globales ont été supprimées.');
   } catch (err) {
-    console.error('❌ Erreur :', err);
+    console.error('❌ Erreur suppression globale :', err);
   }
 })();
