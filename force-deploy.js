04fda84af9f2eba287cdf2266ef727aafa75d1b7
Anderson-Copper <duquesne.pierre@outlook.com>
2025-05-25 00:02:03 +0200

    Delete force-deploy.js

--- force-deploy.js
+++
@@ -1,8 +0,0 @@
-const commands = [
-  new SlashCommandBuilder()
-    .setName('creer-embed')
-    .setDescription('Test simple')
-    .addStringOption(opt =>
-      opt.setName('test').setDescription('champ test').setRequired(true)
-    )
-].map(cmd => cmd.toJSON());
